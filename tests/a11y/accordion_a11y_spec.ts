1548950be5270c532252d7f370ca6fad1356dfb4
Akira Sudoh <asudoh@gmail.com>
2019-11-20 21:07:37 +0900

    test(a11y): attempt to tweak shady DOM

--- tests/a11y/accordion_a11y_spec.ts
+++ tests/a11y/accordion_a11y_spec.ts
@@ -7,10 +7,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { html, render } from 'lit-html';
+import { html, render, TemplateResult } from 'lit-html';
 import testAATCompliance from './test-a11y-compliance';
 import '../../src/components/accordion/accordion';
 import '../../src/components/accordion/accordion-item';
+
+const renderTo = async (content: TemplateResult, container: Element) => {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  render(content, div);
+  await Promise.resolve();
+  container.innerHTML = div.innerHTML.replace(/<bx-/g, '<bx-dummy-').replace(/\/bx-/g, '/bx-dummy-');
+  document.body.removeChild(div);
+};
 
 const template = ({ open }: { open?: boolean } = {}) =>
   html`
@@ -23,18 +32,16 @@ describe('a11y compliance of bx-accordion', function() {
   const container = document.getElementById('html-fragment-container');
 
   it('Should have a11y compliant default state', async function() {
-    render(template(), container!);
-    await Promise.resolve();
+    await renderTo(template(), container!);
     await testAATCompliance(container!, 'bx-accordion: Default state');
   });
 
   it('Should have a11y compliant open state', async function() {
-    render(template({ open: true }), container!);
-    await Promise.resolve();
+    await renderTo(template({ open: true }), container!);
     await testAATCompliance(container!, 'bx-accordion: Open state');
   });
 
   afterEach(function() {
-    render(undefined!, container!);
+    container!.innerHTML = '';
   });
 });
